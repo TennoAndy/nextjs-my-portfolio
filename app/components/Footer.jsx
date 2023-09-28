fcae2d989d91824492ce53f5467658b6d222d6b1
Andreas <andreasjr77@outlook.com>
2023-09-28 04:33:16 +0300

    small fix

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-8 flex justify-between">
+      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <span>
           <Image src="/assets/logo.png" alt="log" width={100} height={100} />
         </span>
