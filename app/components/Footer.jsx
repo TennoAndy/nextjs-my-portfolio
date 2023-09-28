59af54cc59f2c92f2448165d0d0016deb5372b54
Andreas <andreasjr77@outlook.com>
2023-09-28 04:42:22 +0300

    small style fix

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="flex container p-12 lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
+      <div className="flex container p-12 flex-wrap items-center justify-between mx-auto">
         <span>
           <Image src="/assets/logo.png" alt="log" width={100} height={100} />
         </span>
